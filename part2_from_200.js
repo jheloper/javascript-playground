920c16dfb28613dcac80fdd19d23f5163473a5a5
Joonghyeon Kim <jheloper9102@gmail.com>
2020-01-18 23:45:45 +0900

    200제 파트2 숫자, 문자열, 불린, null & undefined 타입에 대한 예제 코드 추가

---
+++ part2_from_200.js
@@ -0,0 +1,23 @@
+console.log(Infinity);
+console.log(1 / Infinity);
+console.log(0 / 0);
+console.log(Infinity - Infinity);
+console.log(0 / "string");
+
+console.log("I'm in jeju");
+console.log('Sewha ocean is wonderful!');
+console.log(`Have you ever eaten Makgeolli?`);
+console.log("This is the first line\nAnd this is the second line");
+
+console.log(7 > 3);
+console.log(7 < 3);
+
+// null
+var value1 = null;
+console.log(value1);
+console.log(typeof value1);
+
+// undefined
+var value2;
+console.log(value2);
+console.log(typeof value2);
