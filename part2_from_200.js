ec0b192e693715a13b305f8d816bcb4fa9cbd31e
Joonghyeon Kim <jheloper9102@gmail.com>
2020-01-23 11:40:08 +0900

    논리 연산자 예제 코드 추가 및 예제 코드들 함수화

--- part2_from_200.js
+++ part2_from_200.js
@@ -1,78 +1,112 @@
-console.log(Infinity);
-console.log(1 / Infinity);
-console.log(0 / 0);
-console.log(Infinity - Infinity);
-console.log(0 / "string");
+// example_nan_infinity();
+// example_string();
+// example_boolean();
+// example_null_undefined();
+// example_template_string();
+// example_arithmetic();
+// example_comparison();
+example_logical();
 
-console.log("I'm in jeju");
-console.log('Sewha ocean is wonderful!');
-console.log(`Have you ever eaten Makgeolli?`);
-console.log("This is the first line\nAnd this is the second line");
+function example_nan_infinity() {
+  console.log(Infinity);
+  console.log(1 / Infinity);
+  console.log(0 / 0);
+  console.log(Infinity - Infinity);
+  console.log(0 / "string");
+}
 
-console.log(7 > 3);
-console.log(7 < 3);
+function example_string() {
+  console.log("I'm in jeju");
+  console.log('Sewha ocean is wonderful!');
+  console.log(`Have you ever eaten Makgeolli?`);
+  console.log("This is the first line\nAnd this is the second line");
+}
 
-// null
-var value1 = null;
-console.log(value1);
-console.log(typeof value1);
+function example_boolean() {
+  console.log(7 > 3);
+  console.log(7 < 3);
+}
 
-// undefined
-var value2;
-console.log(value2);
-console.log(typeof value2);
+function example_null_undefined() {
+  // null
+  var value1 = null;
+  console.log(value1);
+  console.log(typeof value1);
 
-// 템플릿 문자열
-var cart = [
-  { name: "cloth", price: 2000 },
-  { name: "bag", price: 1000 }
-];
+  // undefined
+  var value2;
+  console.log(value2);
+  console.log(typeof value2);
+}
 
-var numOfItems = `number of items in bag: ${cart.length}`;
-var cartTable = 
-`<ul>
+function example_template_string() {
+  // 템플릿 문자열
+  var cart = [
+    { name: "cloth", price: 2000 },
+    { name: "bag", price: 1000 }
+  ];
+
+  var numOfItems = `number of items in bag: ${cart.length}`;
+  var cartTable = `<ul>
     <li>item: ${cart[0].name}, price: ${cart[0].price}</li>
     <li>item: ${cart[1].name}, price: ${cart[1].price}</li>
-</ul>`
-console.log(numOfItems);
-console.log(cartTable);
+</ul>`;
+  console.log(numOfItems);
+  console.log(cartTable);
 
-var personName = 'Richard';
-var helloString = 'Hello ' + personName;
-var helloTemplateString = `Hello ${personName}`;
-console.log(helloString === helloTemplateString);
-console.log(typeof helloTemplateString);
+  var personName = "Richard";
+  var helloString = "Hello " + personName;
+  var helloTemplateString = `Hello ${personName}`;
+  console.log(helloString === helloTemplateString);
+  console.log(typeof helloTemplateString);
+}
 
-// 산술 연산자
-var x = 10;
-x += 5;
-x *= 2;
-console.log(x);
-var y = 10;
-y -= 5;
-y /= 5;
-console.log(y);
-console.log(15 % 4);
-console.log(3 ** 3);
-console.log(+10);
-console.log(-10);
-var numValue = 10;
-++numValue;
---numValue;
-console.log(numValue);
+function example_arithmetic() {
+  // 산술 연산자
+  var x = 10;
+  x += 5;
+  x *= 2;
+  console.log(x);
+  var y = 10;
+  y -= 5;
+  y /= 5;
+  console.log(y);
+  console.log(15 % 4);
+  console.log(3 ** 3);
+  console.log(+10);
+  console.log(-10);
+  var numValue = 10;
+  ++numValue;
+  --numValue;
+  console.log(numValue);
+}
 
-// 비교 연산자
-console.log(5 == 5);
-console.log("5" == 5);
-console.log(5 == '5');
-console.log(5 != 1);
-console.log(5 != "1");
-console.log(5 != "5");
-console.log(5 === 5);
-console.log(5 === "5");
-console.log(5 !== 10);
-console.log(5 !== "5");
-console.log(5 > 3);
-console.log(5 < 3);
-console.log(5 <= 6);
-console.log(5 >= 5);
+function example_comparison() {
+  // 비교 연산자
+  console.log(5 == 5);
+  console.log("5" == 5);
+  console.log(5 == "5");
+  console.log(5 != 1);
+  console.log(5 != "1");
+  console.log(5 != "5");
+  console.log(5 === 5);
+  console.log(5 === "5");
+  console.log(5 !== 10);
+  console.log(5 !== "5");
+  console.log(5 > 3);
+  console.log(5 < 3);
+  console.log(5 <= 6);
+  console.log(5 >= 5);
+}
+
+function example_logical() {
+  // 논리연산자
+  console.log(true && true);
+  console.log(true && false);
+  console.log("text" == "text" && 5 == 5);
+  console.log(5 == 5 && "text1" == "text2");
+
+  console.log(true || false);
+  console.log(false || false);
+  console.log("text" == "text" || 5 == 10);
+}
