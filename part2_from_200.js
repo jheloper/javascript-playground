b0d271a88e144c6a532a5f0767c4980a97662197
Joonghyeon Kim <jheloper9102@gmail.com>
2020-01-24 23:54:16 +0900

    객체의 속성에 접근, 추가, 수정, 삭제하는 예제 코드 추가

--- part2_from_200.js
+++ part2_from_200.js
@@ -8,8 +8,8 @@
 // example_logical();
 // example_ternary();
 // example_bit();
-example_type_casting();
-example_array();
+// example_type_casting();
+// example_array();
 example_object();
 
 function example_nan_infinity() {
@@ -193,4 +193,21 @@ function example_object() {
   family.addFamily(3, "lyn", "niece");
   family.addFamily(10, "dangdangi", "dog");
   console.log(family.getHeadCount());
+
+  var printMembers = function() {
+    var members = family.members;
+    for (role in members) {
+      console.log("role => " + role + ", name => " + members[role].name + ", age => " + members[role].age);
+    }
+  }
+
+  printMembers();
+
+  var members = family.members;
+  members["nephew"] = {age: 3, name: "hyun"};
+  members.niece = {age: 5, name: "lyn"};
+  delete members.aunt;
+  delete members["dog"];
+
+  printMembers();
 }
