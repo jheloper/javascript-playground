659a110d94765d1b57531bac17c988d5d25f15cc
Joonghyeon Kim <jheloper9102@gmail.com>
2020-01-27 17:52:21 +0900

    ES6 비구조화 할당 예제 코드 추가

--- part2_from_200.js
+++ part2_from_200.js
@@ -11,8 +11,9 @@
 // example_type_casting();
 // example_array();
 // example_object();
-example_shorthand_property();
-example_computed_property();
+// example_shorthand_property();
+// example_computed_property();
+example_destructuring_assignment();
 
 function example_nan_infinity() {
   console.log(Infinity);
@@ -235,6 +236,7 @@ function example_shorthand_property() {
 }
 
 function example_computed_property() {
+  // ES6 계산된 속성명
   var obj = {};
   for (var i = 0; i < 4; i++) {
     obj["key" + i] = i;
@@ -247,4 +249,41 @@ function example_computed_property() {
     [profile.split(":")[0]]: profile.split(":")[1]
   };
   console.log(person);
+}
+
+function example_destructuring_assignment() {
+  // 객체 비구조화 할당
+  var obj = {a: 1, b: 2, c: 30, d: 44, e: 5};
+
+  var {a, c} = obj;
+  console.log(`a >>> ${a}`);
+  console.log(`c >>> ${c}`);
+
+  var {a:newA=10, f:newF=5} = obj;
+  console.log(`newA >>> ${newA}`);
+  console.log(`newF >>> ${newF}`);
+
+  // 배열 비구조화 할당
+  var arr = [1, 2, 30, 44, 5];
+
+  var [b, c, ...rest] = arr;
+  console.log(`0) b >>> ${b}`);
+  console.log(`0) c >>> ${c}`);
+  console.log(`0) rest >>> ${rest}`);
+
+  var [a=10, f=9] = [1];
+  console.log(`1) a >>> ${a}`);
+  console.log(`1) f >>> ${f}`);
+
+  [a, f] = [f, a];
+  console.log(`2) a >>> ${a}`);
+  console.log(`2) f >>> ${f}`);
+
+  function getArr() {
+    return [1, 2, 3, 4, 5, 6];
+  }
+
+  [a, , , , , f] = getArr();
+  console.log(`3) a >>> ${a}`);
+  console.log(`3) f >>> ${f}`);
 }
