e958e792eccf2fcc31d8b44f32a58316bca9e9f1
Joonghyeon Kim <jheloper9102@gmail.com>
2020-02-12 23:32:11 +0900

    클래스 정적 메서드와 정적 속성 예제 코드 추가

--- part2_2_from_200.js
+++ part2_2_from_200.js
@@ -10,8 +10,9 @@
 // example_oop();
 // example_constructor();
 // example_inheritance_prototype();
-example_class();
-example_inheritance_class();
+// example_class();
+// example_inheritance_class();
+example_class_static();
 
 function example_scope() {
   var a = 10;
@@ -402,4 +403,51 @@ function example_inheritance_class() {
 
   const barChart1 = new BarChart(100, 100);
   barChart1.draw();
+}
+
+function example_class_static() {
+  class Product {
+    static build(name, price) {
+      const id = Math.floor(Math.random() * 1000);
+      return new Product(id, name, price);
+    }
+
+    static getTaxPrice(product) {
+      return (product.price * 0.1) + product.price;
+    }
+
+    constructor(id, name, price) {
+      this.id = id;
+      this.name = name;
+      this.price = price;
+    }
+  }
+
+  class DeposableProduct extends Product {
+    depose() {
+      this.deposed = true;
+    }
+  }
+
+  const gum = Product.build("gum", 1000);
+  console.log(gum);
+
+  const clothes = new DeposableProduct(1, "clothes", 2000);
+  const taxPrice = DeposableProduct.getTaxPrice(clothes);
+  console.log(taxPrice);
+
+  class ProductWithCode {
+    static get CODE_PREFIX() {
+      return "PRODUCT-";
+    }
+
+    constructor(id) {
+      this.id = id;
+      this.code = ProductWithCode.CODE_PREFIX + id;
+    }
+  }
+
+  const product1 = new ProductWithCode("001");
+  console.log(ProductWithCode.CODE_PREFIX);
+  console.log(product1.code);
 }
