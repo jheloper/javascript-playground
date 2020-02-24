af4df94fa7cc2738bfa8f3bf76b9214eb091e593
Joonghyeon Kim <jheloper9102@gmail.com>
2020-02-24 20:36:41 +0900

    typeof, instanceof 연산자를 이용하여 자료형 확인하는 예제 코드 추가

--- example_200_for_starter/part3.js
+++ example_200_for_starter/part3.js
@@ -1,4 +1,5 @@
-example_standard_builtin_object();
+// example_standard_builtin_object();
+example_typeof_instanceof();
 
 function example_standard_builtin_object() {
     const str = new String("Javascript");
@@ -23,4 +24,42 @@ function example_standard_builtin_object() {
 
     console.log(str1.valueOf());
     console.log(str2.valueOf());
+}
+
+function example_typeof_instanceof() {
+    const str = "Javascript";
+    const strObj = new String("Javascript");
+    const num = 200;
+    const numObj = new Number(200);
+    const bool = true;
+    const boolObj = new Boolean(true);
+    const func = function() {};
+    const arr = [10, 200, 4000];
+    const obj = { a1: "test" };
+    const empty = null;
+    const notCalled = undefined;
+
+    console.log(typeof str === "string");
+    console.log(typeof strObj === "object");
+    console.log(typeof num === "number");
+    console.log(typeof numObj === "object");
+    console.log(typeof bool === "boolean");
+    console.log(typeof boolObj === "object");
+    console.log(typeof func === "function");
+    console.log(typeof arr === "object");
+    console.log(typeof obj === "object");
+    console.log(typeof empty === "object");
+    console.log(typeof notCalled === "undefined");
+
+    console.log(str instanceof String);
+    console.log(strObj instanceof String);
+    console.log(num instanceof Number);
+    console.log(numObj instanceof Number);
+    console.log(bool instanceof Boolean);
+    console.log(boolObj instanceof Boolean);
+    console.log(arr instanceof Array);
+    console.log(obj instanceof Object);
+    console.log(func instanceof Function);
+    console.log(empty instanceof Object);
+    console.log(notCalled instanceof undefined);
 }
