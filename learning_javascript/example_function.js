1bde4f201d7a19e5deb42dda5d283459da0cb927
Joonghyeon Kim <jheloper9102@gmail.com>
2020-05-24 23:55:51 +0900

    러닝 자바스크립트 함수 매개변수에 대한 예제 코드 추가

--- learning_javascript/example_function.js
+++ learning_javascript/example_function.js
@@ -15,3 +15,15 @@ console.log(o.f());
 const arr = [1, 2, 3];
 arr[1] = getGreeting;
 console.log(arr[1]());
+
+// 함수의 매개변수는 함수의 참조가 아니라 값이 전달된다.
+let x = 3;
+console.log(`함수 호출 전 : x = ${x}`);
+changeValue(x);
+console.log(`함수 호출 후 : x = ${x}`);
+
+function changeValue(x) {
+    console.log(`함수 내부 : x = ${x}`);
+    x = 5;
+    console.log(`함수 내부(값 변경 후) : x = ${x}`);
+}
