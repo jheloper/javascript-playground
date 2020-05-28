b2965f3098bc68dce11005af86985e8c465f41ba
Joonghyeon Kim <jheloper9102@gmail.com>
2020-05-28 23:55:09 +0900

    200제 파트3 배열 length 예제 코드 추가

--- example_200_for_starter/part3.js
+++ example_200_for_starter/part3.js
@@ -16,8 +16,9 @@
 // example_string_toLowerCase_and_toUpperCase();
 // example_array_from();
 // example_string_split();
-example_array_push();
-example_array_unshift();
+// example_array_push();
+// example_array_unshift();
+example_array_length();
 
 function example_standard_builtin_object() {
     const str = new String("Javascript");
@@ -347,4 +348,26 @@ function example_array_unshift() {
     festa.forEach((name) => {
         console.log(name);
     });
+}
+
+function example_array_length() {
+    const ship = {
+        max: 4,
+        passengers: [],
+        onBoard: function(name) {
+            if (this.passengers.length === 4) {
+                console.log(`This ship is full. ${name} can not board this ship.`);
+            } else {
+                this.passengers.push(name);
+                console.log(`${name} boarded.`);
+            }
+        }
+    }
+
+    ship.onBoard("chloe");
+    ship.onBoard("jay");
+    ship.onBoard("david");
+    ship.onBoard("asher");
+    ship.onBoard("daniel");
+    console.log(ship.passengers);
 }
